9dc5e85c7a5fa236032b96cd89b784ca407bc606
Dokthoror <pascalguyguy@gmail.com>
2021-03-14 17:56:11 +0100

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./src/index.css"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
