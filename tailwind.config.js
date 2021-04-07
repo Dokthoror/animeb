837eeda3e3c5ca38f7808168d0d695c9ab78cf53
Dokthoror <pascalguyguy@gmail.com>
2021-04-07 11:10:37 +0200

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        maxHeight: {
+            "0": "0",
+            "1/4": "25%",
+            "1/2": "50%",
+            "3/4": "75%",
+            full: "100%",
+        },
+        minHeight: {
+            "0": "0",
+            "1/4": "25%",
+            "1/2": "50%",
+            "3/4": "75%",
+            full: "100%",
+        },
     },
     variants: {
         extend: {},
